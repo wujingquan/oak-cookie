f4b10837b48e9fed2e7f961da833c58b836ce969
wujingquan <i@wujingquan.com>
2020-01-01 22:42:26 +0800

    add

--- mod.ts
+++ mod.ts
@@ -1,6 +1,35 @@
-import * as cookie from "https://deno.land/std/http/cookie.ts";
+import {
+  Cookie,
+  getCookies,
+  setCookie,
+  delCookie
+} from "https://deno.land/std/http/cookie.ts";
 
 export default async function(ctx, next) {
-  ctx.state.cookies = { ...cookie }
+  ctx.state.cookies = {
+    gets () {
+      return getCookies(ctx.request)
+    },
+    get (name) {
+      
+    },
+    set (_cookie: Cookie) {
+      const cookie: Cookie = {
+        expires: new Date(),
+        maxAge: 123,
+        domain: '',
+        path: '/',
+        secure: true,
+        httpOnly: true,
+        // sameSite?: SameSite;
+        // unparsed?: string[];
+        ..._cookie
+      }
+      return setCookie(ctx.response, cookie)
+    },
+    del (name) {
+      return delCookie(ctx.response, name)
+    }
+  }
   await next();
 }
